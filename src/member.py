948949c82283c4bc496b2b317be2f7325bde0285
Krzysztof Greczka <krzysztof.gre@protonmail.com>
2020-04-18 17:02:30 +0200

    refactor

--- src/member.py
+++ src/member.py
@@ -2,36 +2,39 @@ import random
 
 
 class Member:
-    def __init__(self, id, raw_input):
-        self.id = id
+    def __init__(self, member_id, raw_input, crossover_method, mutation_method):
+        # not sure if id is needed
+        self.member_id = member_id
         # raw_input - info from STM pins
         self.input  # = parse raw_input to structural input
+        self.crossover_method = crossover_method
+        self.mutation_method = mutation_method
+        self.fitness = 0
 
     def calculate_fitness(self):
-        pass
         # calculate fitness
-        # self.fitness = calculated fitness
-        # return self.fitness
+        self.fitness = len(self.input)  # primitive calculating method
+        return self.fitness
 
-    def mutate(self, operator):
+    def mutate(self):
         # self.input - list
         i = 0
         j = 0
         while j - i < 2:
             i = random.randint(0, len(self.input))
             j = random.randint(i, len(self.input))
-        if operator == 1:  # random resetting - set random element to 0
+        if self.mutation_method == 1:  # random resetting - set random element to 0
             self.input[i] = 0
-        elif operator == 2:  # swap mutation - swap two elements
+        elif self.mutation_method == 2:  # swap mutation - swap two elements
             self.input[i], self.input[j] = self.input[j], self.input[i]
-        elif operator == 3:  # scramble mutation - shuffle random part
+        elif self.mutation_method == 3:  # scramble mutation - shuffle random part
             new_list = self.input[i:j]
             random.shuffle(new_list)
             self.input[i:j] = new_list
-        elif operator == 4:  # inversion mutation - invert random part
+        elif self.mutation_method == 4:  # inversion mutation - invert random part
             self.input[i:j] = list(reversed(self.input[i:j]))
 
-    def crossover(self, operator, parent):
+    def crossover(self, parent):
         # returns a child
         # self.input - list
         i = 0
@@ -42,7 +45,7 @@ class Member:
                 length = len(parent.input)
             i = random.randint(0, length)
             j = random.randint(i, length)
-        if operator == 1:  # one point
+        if self.crossover_method == 1:  # one point
             return self.input[:i] + parent.input[i:]
-        elif operator == 2:  # multi point
+        elif self.crossover_method == 2:  # multi point
             return self.input[:i] + parent.input[i:j] + self.input[j:]
