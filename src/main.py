cd279f4b68297bfbfc23e294118e43c9e8cc2bbc
Krzysztof Greczka <krzysztof.gre@protonmail.com>
2020-05-02 12:24:50 +0200

    implemented new constructor

--- src/main.py
+++ src/main.py
@@ -2,17 +2,18 @@ from src.input import Operator
 from src.population import Population
 
 if __name__ == '__main__':
-    population = Population()
 
     generations = 10
 
     # config population
-    population.operator = Operator([-1, 1])  # x^2 -1
-    population.population_size = 100
-    population.population_discard = 0.2
-    population.noise = 0.1
-    population.crossover_options = [1, 2]
-    population.mutation_options = [1, 2, 3, 4]
+    operator = Operator([-1, 1])  # x^2 -1
+    population_size = 100
+    population_discard = 0.2
+    noise = 0.1
+    mutation_options = [1, 2, 3, 4]
+    crossover_options = [1, 2]
+
+    population = Population(operator, population_size, population_discard, noise, mutation_options, crossover_options)
 
     for x in range(0, generations):
         best_result = population.new_gen(population)
