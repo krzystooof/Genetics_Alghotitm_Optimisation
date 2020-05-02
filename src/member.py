cd449ff982928240c15c71f32bbe6f80aeb1f83b
Krzysztof Greczka <krzysztof.gre@protonmail.com>
2020-05-02 11:29:57 +0200

    implemented Operator

--- src/member.py
+++ src/member.py
@@ -2,48 +2,48 @@ import random
 
 
 class Member:
-    def __init__(self, raw_input, crossover_method, mutation_method):
+    def __init__(self, operator):
         # raw_input - info from STM pins
-        self.input  # TODO = parse raw_input to structural input
-        self.crossover_method = crossover_method
         self.fitness = 0
+        self.operator = operator
 
-    def calculate_fitness(self):
-        # TODO
-        # calculate fitness
-        self.fitness = len(self.input)  # primitive calculating method
-        return self.fitness
+    # calculating outside the code
+    # def calculate_fitness(self):
+    #     # TODO
+    #     # calculate fitness
+    #     self.fitness = len(self.input)  # primitive calculating method
+    #     return self.fitness
 
     def mutate(self, mutation_method):
         # self.input - list
         i = 0
         j = 0
         while j - i < 2:
-            i = random.randint(0, len(self.input))
-            j = random.randint(i, len(self.input))
+            i = random.randint(0, len(self.operator.values))
+            j = random.randint(i, len(self.operator.values))
         if mutation_method == 1:  # random resetting - set random element to 0
-            self.input[i] = 0
+            self.operator.values[i] = 0
         elif mutation_method == 2:  # swap mutation - swap two elements
-            self.input[i], self.input[j] = self.input[j], self.input[i]
+            self.operator.values[i], self.operator.values[j] = self.operator.values[j], self.operator.values[i]
         elif mutation_method == 3:  # scramble mutation - shuffle random part
-            new_list = self.input[i:j]
+            new_list = self.operator.values[i:j]
             random.shuffle(new_list)
-            self.input[i:j] = new_list
+            self.operator.values[i:j] = new_list
         elif mutation_method == 4:  # inversion mutation - invert random part
-            self.input[i:j] = list(reversed(self.input[i:j]))
+            self.operator.values[i:j] = list(reversed(self.operator.values[i:j]))
 
     def crossover(self,crossover_method , parent):
         # returns a child
-        # self.input - list
+        # self.operator.values - list
         i = 0
         j = 0
         while j - i < 2:
-            length = len(self.input)
+            length = len(self.operator.values)
             if len(parent.input) < length:
-                length = len(parent.input)
+                length = len(parent.operator.values)
             i = random.randint(0, length)
             j = random.randint(i, length)
         if crossover_method == 1:  # one point
-            return self.input[:i] + parent.input[i:]
+            return self.operator.values[:i] + parent.operator.values[i:]
         elif crossover_method == 2:  # multi point
-            return self.input[:i] + parent.input[i:j] + self.input[j:]
+            return self.operator.values[:i] + parent.operator.values[i:j] + self.operator.values[j:]
