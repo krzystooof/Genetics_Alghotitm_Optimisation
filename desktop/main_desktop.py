481fb327551473e697fa17b7bc11941c11b1e880
Krzysztof Greczka <krzysztof.gre@protonmail.com>
2020-05-12 18:24:24 +0200

    rebuilded to implement new GUI

--- desktop/main_desktop.py
+++ desktop/main_desktop.py
@@ -1,9 +1,7 @@
 from desktop.gui import GUI
-from desktop.usb import USB
-from desktop.alghoritm_control import *
+from desktop.alghoritm_control import Controller
 
 if __name__ == '__main__':
-    usb = USB()
     gui = GUI("Desktop STM GA Control Panel")
 
     gui.add_text_entry("PyBoard port:")
@@ -16,18 +14,21 @@ if __name__ == '__main__':
     gui.add_checkboxes("Population reverse fitness:", [""], False)
     gui.add_checkboxes("Member mutation options:", ["Random resetting", "Swap", "Scramble", "Inversion"], False)
     gui.add_checkboxes("Member crossover options:", ["One point", "Multi point"], False)
+    # rows with checkboxes that must have at least one option set
+    checkboxes_one_set = [7, 8]
 
     gui.add_console()
 
-    checkboxes_one_set = [7, 8]
-    start_button_action = lambda: start_algorithm(gui, usb, checkboxes_one_set)
+    controller = Controller(gui, checkboxes_one_set)
+
+    start_button_action = lambda: controller.start_algorithm()
     gui.add_button("START", start_button_action)
-    stop_button_action = lambda: stop_algorithm(gui, usb)
+    stop_button_action = lambda: controller.stop_algorithm()
     gui.add_button("STOP", stop_button_action)
-    restart_button_action = lambda: restart_algorithm(gui, usb)
+    restart_button_action = lambda: controller.restart_algorithm()
     gui.add_button("RESTART", restart_button_action)
-    stop_button_action = lambda: stop_algorithm(gui, usb)
-    gui.add_button("START", stop_button_action)
+    pause_button_action = lambda: controller.pause_algorithm()
+    gui.add_button("PAUSE", pause_button_action)
 
     # must be at the end of main
     gui.work()
