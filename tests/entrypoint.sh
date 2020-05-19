20b17d0d866f611cef4276fe19e8f3ea0272be69
Jakub Chodubski <jchodubski@pm.me>
2020-05-19 09:57:55 +0200

    Commented config test, uncommented coverage test

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -7,8 +7,8 @@ octave-cli ga_example.m > results_octave.txt
 python main_tests.py
 
 # Run unit tests
-# python -m coverage run -m unittest discover  # TODO Uncomment once updated tests
-# python -m coverage report
+python -m coverage run -m unittest discover
+python -m coverage report
 
 # Compare given results
 python compare.py
