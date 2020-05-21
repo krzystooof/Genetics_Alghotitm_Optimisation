c11e8eb9398b8592bc39e69162e1dde4e1993fde
Jakub Chodubski <jchodubski@pm.me>
2020-05-21 18:31:52 +0200

    Auto port detection on linux

--- desktop/usb.py
+++ desktop/usb.py
@@ -10,9 +10,17 @@ class USB:
     @author: Jakub Chodubski
     """
 
-    def __init__(self, comm_port='/dev/ttyACM0'):  # ttyACM1 is default pyboard port in Linux
+    def __init__(self, comm_port=None):
         """Initiates connection. Waits till connected."""
-        self.usb = serial.Serial(comm_port, 9600, timeout=0)
+        if comm_port is None:
+            # Defaults to one of 2 ports
+            try:
+                self.usb = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
+            except serial.serialutil.SerialException:
+                self.usb = serial.Serial('/dev/ttyACM1', 9600, timeout=0)
+        else:
+            self.usb = serial.Serial(comm_port, 9600, timeout=0)
+
         self.dictionary = dict()
         while not self.usb.is_open:
             self.usb.open()
@@ -41,7 +49,10 @@ class USB:
             while read_string.count('{') != read_string.count('}'):
                 read = self.usb.readline()
                 read_string += read.decode('utf-8')
-            return json.loads(read_string)
+            try:
+                return json.loads(read_string)
+            except json.decoder.JSONDecodeError:
+                raise ValueError(read_string)
         else:
             return {'type': 0}
 
