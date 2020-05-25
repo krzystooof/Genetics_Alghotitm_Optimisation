f238aac0545d7b4250be801c7a5fb34b377abcb7
Grzegorz Drozda <gregordr123@gmail.com>
2020-05-25 13:30:00 +0200

    Add test of function

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,6 +1,9 @@
 from pyb.src.algorithm import Population
 import json
 
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
     config = get_config()
@@ -29,6 +32,7 @@ def get_config():
     mutate = [1, 2, 3, 4]
     crossover = [1, 2]
     config_dict = {
+        'generations': 1000,
         'population_size': 100,
         'population_discard': 0.5,
         'population_noise': 0.1,
@@ -37,7 +41,7 @@ def get_config():
         'member_config': {
             'random_low': -100,
             'random_high': 100,
-            'num_values': 1,
+            'num_values': 2,
             'mutation_options': mutate,
             'crossover_options': crossover
             }
@@ -45,9 +49,66 @@ def get_config():
     }
     return config_dict
 
+minimum = 10000
+maximum = 0.0
+counter = 0
 
 def get_fitness(operator):  # simpleFitFunc, but in python
-    return (operator.values[0] - 1.0)*(operator.values[0] - 1.0)
+    # model
+    M = np.array([1.995908286, -0.996008084, 0.000024949, 0.000049899, 0.000024949])
 
+    # simulation time
+    tsim = 30  # [s]
+
+    # sample time
+    ts = 0.01  # [s]
+
+    # time vector
+    t = np.arange(0, tsim, ts)  # [s]
+
+    # model output signal
+    y = np.zeros(t.size)
+
+    # model input signal
+    u = np.zeros(t.size)
+
+    # model reference signal
+    r = np.ones(t.size)
+
+    # controller gains
+    Kp = operator.values[0]
+    Ki = operator.values[1]
+    ei = 0
+
+    # simulation loop
+    for i in range(2, t.size):
+        e = r[i - 1] - y[i - 1]
+        ei = ei + e
+        u[i] = Kp * e + Ki * ei * ts
+        y[i] = M.dot(np.array([y[i - 1], y[i - 2], u[i], u[i - 1], u[i - 2]]).transpose())
+
+    q = math.sqrt(np.sum(np.square(r - y)) / t.size)
+
+    # plot results - disable for optimization !!!
+    # fig = plt.figure()
+    # ax = fig.gca()
+    # ax.grid(True)
+    # ax.set_ylabel('Output signal [-]')
+    # ax.set_xlabel('Time [s]')
+    # plt.plot(t, y)
+    # plt.show()
+    global minimum
+    global maximum
+    if q < minimum:
+        minimum = q
+    if q > maximum:
+        maximum = q
+    print(q)
+    print("Minimum: ", minimum)
+    print("Maximum: ", maximum)
+    global counter
+    counter = counter + 1
+    print("Counter: ", counter)
+    return q
 
 main()
