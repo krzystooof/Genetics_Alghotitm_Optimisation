bb7551f4aaed688241adc7464f049a82d4d94aa6
Jakub Chodubski <jchodubski@pm.me>
2020-05-27 09:29:01 +0200

    Updated guideline

--- tests/main_alg.py
+++ tests/main_alg.py
@@ -1,10 +1,12 @@
 import pyb.src.algorithm
 
+
 def function(x):
     # x^2 + 3x +7
     return x[0] * x[0] + x[0] * 3 + 7
 
-alg = Algorithm(function, values = 1, population = 100, generations = 15, rand_low = -100, rand_high = 100, reverse = True)
+
+alg = Algorithm(function, values=1, population=100, accuracy=0.0001, rand_low=-100, rand_high=100, reverse=True)
 
 result = agl.run()
 print("This function has a minimum of: " + str(result))
