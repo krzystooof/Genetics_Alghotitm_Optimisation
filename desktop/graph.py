4899c3e31ddea3faecedaa29ac5c6e764ab9db86
Krzysztof Greczka <krzysztof.gre@protonmail.com>
2020-06-01 10:49:58 +0200

    added ability to load data from file

--- desktop/graph.py
+++ desktop/graph.py
@@ -1,10 +1,12 @@
 """This module contains code used to graphically present data on the graph"""
+import json
+
 import plotly.graph_objects as go
 
 
 class Graph:
     """
-    Graph creating and showing
+    Graph creating and displaying
     @author: Krzysztof Greczka
     """
 
@@ -18,8 +20,32 @@ class Graph:
             self.x.append(str(i + 1) + "[" + str(additonal_x_info[i]) + "]")
 
     # mode = ['lines','lines+markers', 'markers']
-    def add_y_axis_data(self, data_name, data, mode):
+    def add_y_axis_data(self, data_name, data, mode='lines+markers'):
         self.figure.add_trace(go.Scatter(x=self.x, y=data, mode=mode, name=data_name))
+
+    def load_y_axis_data(self, filename,x_entry, data, mode='lines+markers'):
+        figures = []
+        names = []
+        for i in range(len(data)):
+            figures.append([])
+            names.append(data[i])
+        with open(filename, "r") as file:
+            for line in file:
+                try:
+                    result = json.loads(line)
+                    self.x.append(result[x_entry])
+                    for x in range(len(data)):
+                        if result[data[x]]:
+                            figures[x].append(result[data[x]])
+                except ValueError or json.decoder.JSONDecodeError:
+                    print("Could not read line: "+line)
+                except KeyError as e:
+                    print("Line: " + line+"Cannot find: " + str(e))
+        for y in range(len(figures)):
+            self.add_y_axis_data(names[y], figures[y], mode)
 
     def show(self):
         self.figure.show()
+
+
+
